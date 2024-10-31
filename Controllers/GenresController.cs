80bd1025a954a4a1e782c9c8fef371142511d66c
GUSTAVO CESAR MEIRINHO <gustavo.meirinho@alunos.sc.senac.br>
2024-10-31 17:01:23 -0300

    feat: Add database conection

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,21 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class GenresController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Genre> genres = new List<Genre>
+            {
+                new Genre(1, "Drama"),
+                new Genre(2, "Comédia"),
+                new Genre(3, "Terror"),
+                new Genre(4, "Ação")
+            };
+
+            return View(genres);
+        }
+    }
+}

4b32335713ab3fb0d18693270d2504329e64c147
Cesar <gustavocesarmeirinho3@gmail.com>
2024-11-01 18:51:37 -0300

    Reenviando "Data"

--- Data/BookstoreContext.cs
+++ Data/BookstoreContext.cs
@@ -1,8 +1,14 @@
-﻿namespace Bookstore.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Bookstore.Models;
+
+namespace Bookstore.Data
 {
     public class BookstoreContext : DbContext
     {
-    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Genre> Genres { get; set; }
     }
-}
 }
